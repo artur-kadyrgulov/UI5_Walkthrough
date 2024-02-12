14b0e03e2f47e26222b72363f1bb7b82d0865e3d
Artur Kadyrgulov <arturkadyrgulov@gmail.com>
2024-02-12 19:05:13 +0300

    Step 8: Translatable Texts

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
- ], (Controller, MessageToast, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+ ], (Controller, MessageToast, JSONModel,ResourceModel) => {
     "use strict";
  
     return Controller.extend("kadyrgulov.walkthrough.controller.App", {
@@ -15,10 +16,21 @@ sap.ui.define([
             };
             const oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+
+            const i18nModel = new ResourceModel({
+                bundleName: "kadyrgulov.walkthrough.i18n.i18n"
+            });
+            this.getView().setModel(i18nModel, "i18n");
+
         },
 
         onShowHello() {
-            MessageToast.show("Hello World");
+            // read msg from i18n model
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+            MessageToast.show(sMsg);
         }
     });
 
